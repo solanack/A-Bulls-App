cdf648f1f5da423cb3c93a85329e6dd0867c607f
ImADingDong <93940971+solanack@users.noreply.github.com>
2026-08-24 16:34:21 -0400

    Bump Pages release to 8.6.1

--- js/core.js
+++ js/core.js
@@ -36,7 +36,7 @@
     .replaceAll('>', '&gt;').replaceAll('"', '&quot;').replaceAll("'", '&#039;');
 
   const Platform = {
-    version: '8.6.0',
+    version: '8.6.1',
     workerRequired: '8.1.0',
     buildStamp() {
       return `PAGES ${this.version} • WORKER ${this.workerRequired}`;
